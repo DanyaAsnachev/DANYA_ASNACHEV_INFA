47c1ab781bdc441147732f457e6e780d31a63196
DanyaAsnachev <144052663+DanyaAsnachev@users.noreply.github.com>
2023-10-14 18:09:39 +0300

    Sem5dz5z.c

--- Sem5/Sem5dz5z.c
+++ Sem5/Sem5dz5z.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main()
 {
-	char a[1000], b = '!';
+	char a[1000], b = '!', k = 0, c[1000];
 	scanf("%[^\n]", a);
 
 	for (int i = 0; a[i]; i++)
 	{
 		if (a[i] == ' ')
 		{
-			a[i] = b;
+			c[i + k] = b;
+			k += 1;
 		}
+		c[i + k] = a[i];
 	}
 	
-	printf("%s", a);
+	printf("%s", c);
 	printf("%c\n", b);
 }
